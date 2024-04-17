746b0331cf7e7cfdb24f63833407e977d4a09b0e
Vinay Rajput <105452265+Rajput-vinay@users.noreply.github.com>
2024-04-17 22:50:13 +0530

    Update emailVerificationTemplate.js

--- SERVER/mail/templates/emailVerificationTemplate.js
+++ SERVER/mail/templates/emailVerificationTemplate.js
@@ -67,7 +67,7 @@ const otpTemplate = (otp) => {
 	<body>
 		<div class="container">
 			<a href="https://vidya-sankalp.vercel.app/"><img class="logo"
-					src="https://ibb.co/28YxFb0" alt="VidyaSankalp Logo"></a>
+					src="https://i.ibb.co/Fsq2hPk/rzp-logo.png" alt="VidyaSankalp Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
